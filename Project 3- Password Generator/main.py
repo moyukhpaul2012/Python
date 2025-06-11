8ea324855fb42dcaea3d8e3fc2d2c520e73452a4
Moyukh Paul <98570432+moyukhpaul2012@users.noreply.github.com>
2025-06-11 22:49:24 +0530

    Delete Project 3- Password Generator directory

--- Project 3- Password Generator/main.py
+++
@@ -1,9 +0,0 @@
-import random
-
-chars= "abcdefghijklmnopqrstuvwxyz0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()"
-length= int(input("enter length of password: "))
-password=""
-
-for i in range(length):
-    password+=random.choice(chars)
-print(password)
